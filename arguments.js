a485a826d40c86f244b9841c733f2bda9a44ee8d
JeffreyMJordan <jeff2016@alumni.stanford.edu>
2017-11-06 13:28:09 -0800

    Adds arguments.js

---
+++ arguments.js
@@ -0,0 +1,107 @@
+const sum = function(){
+  let res = 0;
+  for(var i = 0; i<arguments.length; i++){
+    res += arguments[i];
+  }
+  return res;
+};
+
+ Function.prototype.myBind = function(context, ...bindArgs){
+  console.log(bindArgs);
+  console.log(context);
+  return (...callArgs) => {
+    console.log(callArgs);
+    return this.apply(context, bindArgs.concat(callArgs));
+  };
+};
+
+
+class Cat {
+  constructor(name) {
+    this.name = name;
+  }
+
+  says(sound, person) {
+    console.log(`${this.name} says ${sound} to ${person}!`);
+    return true;
+  }
+}
+
+// const markov = new Cat("Markov");
+// const breakfast = new Cat("Breakfast");
+
+// markov.says("meow", "Ned");
+// // Markov says meow to Ned!
+// // true
+//
+// // bind time args are "meow" and "Kush", no call time args
+// markov.says.myBind(breakfast, "meow", "Kush")();
+// // Breakfast says meow to Kush!
+// // true
+//
+// markov.says.myBind(breakfast)("meow", "a tree");
+//
+//
+// // bind time arg is "meow", call time arg is "Markov"
+// markov.says.myBind(breakfast, "meow")("Markov");
+// // Breakfast says meow to Markov!
+// // true
+//
+// // no bind time args (other than context), call time args are "meow" and "me"
+// const notMarkovSays = markov.says.myBind(breakfast);
+// notMarkovSays("meow", "me");
+// // Breakfast says meow to me!
+// // true
+
+const curriedSum = function(numArgs) {
+  const numbers = [];
+  const _curriedSum = function(arg) {
+    numbers.push(arg);
+
+    if (numbers.length === numArgs) {
+      let finalSum = 0;
+      numbers.forEach(el => {
+        finalSum += el;
+      });
+      return finalSum;
+    } else {
+      return _curriedSum;
+    }
+  };
+  return _curriedSum;
+
+};
+
+// let sumFn = curriedSum(3);
+// console.log(sumFn(1));
+// console.log(sumFn(2));
+// console.log(sumFn(3));
+
+
+
+Function.prototype.curry = function(numArgs){
+  const args = [];
+  const fn = this;
+  const _curry = function(arg){
+    args.push(arg);
+
+    if (args.length === numArgs) {
+      return fn.apply( fn, args);
+    } else {
+      return _curry;
+    }
+  };
+  return _curry;
+};
+
+
+const func = function(){
+  for(var i = 0; i<arguments.length; i++){
+    console.log(arguments[i]);
+  }
+};
+
+const curryFunc = func.curry(3);
+console.log(curryFunc(1));
+console.log(curryFunc(2));
+console.log(curryFunc(3));
